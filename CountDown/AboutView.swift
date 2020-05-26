e9983b397cb1206260be1eb59022c2ff2eca5c21
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-05-26 21:31:33 +0300

    🚀 Smol fixes and tweaks

---
+++ CountDown/AboutView.swift
@@ -0,0 +1,21 @@
+//
+//  AboutView.swift
+//  Pomosh
+//
+//  Created by Steven J. Selcuk on 23.05.2020.
+//  Copyright © 2020 Steven J. Selcuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
---
+++ CountDown/TooltipComponent.swift
@@ -0,0 +1,21 @@
+//
+//  TooltipComponent.swift
+//  Pomosh
+//
+//  Created by Steven J. Selcuk on 26.05.2020.
+//  Copyright © 2020 Steven J. Selcuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct TooltipComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TooltipComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        TooltipComponent()
+    }
+}
