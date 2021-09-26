2d70d3b51a8f79293c781ab7cad4d056188e4d16
Steven J. Selcuk <stevenjselcuk@gmail.com>
2021-09-26 16:04:18 +0300

    Fixes issue #11

--- Pomosh/AppDelegate.swift
+++ Pomosh/AppDelegate.swift
@@ -8,20 +8,20 @@
 
 import AppKit
 import Cocoa
-//import HotKey
+// import HotKey
 import SwiftUI
 import UserNotifications
 
-//let toggleTimerHotkey = HotKey(key: .p, modifiers: [.command, .control])
+// let toggleTimerHotkey = HotKey(key: .p, modifiers: [.command, .control])
 // let pauseHotkey = HotKey(key: .s, modifiers: [.command, .control])
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     @ObservedObject var PoTimer = PomoshTimer()
     var popover: NSPopover!
+    let invisibleWindow = NSWindow(contentRect: NSMakeRect(0, 0, 20, 5), styleMask: .borderless, backing: .buffered, defer: false)
 
-    var statusBarItem: NSStatusItem! = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength + ( PomoshTimer().showMenubarTimer ? 70 : 0)))
-  
+    var statusBarItem: NSStatusItem! = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength + (PomoshTimer().showMenubarTimer ? 70 : 0)))
 
     // Init default variables for first launch
     let userDefaultsDefaults = [
@@ -35,9 +35,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         //  UserDefaults.standard.register(defaults: userDefaultsDefaults)
         // Create the SwiftUI view that provides the window contents.
+
+        invisibleWindow.backgroundColor = .red
+        invisibleWindow.alphaValue = 0
         let contentView = ContentView()
-        NSMenu.setMenuBarVisible(true)
-        // Create the popover
+
         let popover = NSPopover()
         popover.contentSize = NSSize(width: 400, height: 400)
         popover.behavior = .transient
@@ -51,11 +53,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let button = statusBarItem.button {
             button.image = NSImage(named: "menubar-icon")
             button.imagePosition = NSControl.ImagePosition.imageLeft
-           if self.PoTimer.showMenubarTimer == true {
-                button.title = String(self.PoTimer.textForPlaybackTime(time: TimeInterval(PoTimer.timeRemaining)))
-            button.font = NSFont.monospacedDigitSystemFont(ofSize: 12.0, weight: NSFont.Weight.medium)
+            if PoTimer.showMenubarTimer == true {
+                button.title = String(PoTimer.textForPlaybackTime(time: TimeInterval(PoTimer.timeRemaining)))
+                button.font = NSFont.monospacedDigitSystemFont(ofSize: 12.0, weight: NSFont.Weight.medium)
             }
-           
+
             button.action = #selector(togglePopover(_:))
             button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
@@ -77,7 +79,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 if popover.isShown {
                     popover.performClose(sender)
                 } else {
-                    popover.show(relativeTo: sbutton.bounds, of: sbutton, preferredEdge: NSRectEdge.minY)
+                    // find the coordinates of the statusBarItem in screen space
+                    let buttonRect: NSRect = sbutton.convert(sbutton.bounds, to: nil)
+                    let screenRect: NSRect = sbutton.window!.convertToScreen(buttonRect)
+
+                    // calculate the bottom center position (10 is the half of the window width)
+                    let posX = screenRect.origin.x + (screenRect.width / 2) - 10
+                    let posY = screenRect.origin.y
+
+                    // position and show the window
+                    invisibleWindow.setFrameOrigin(NSPoint(x: posX, y: posY))
+                    invisibleWindow.makeKeyAndOrderFront(self)
+                    NSApplication.shared.presentationOptions = []
+                    // position and show the NSPopover
+                    popover.show(relativeTo: invisibleWindow.contentView!.frame, of: invisibleWindow.contentView!, preferredEdge: NSRectEdge.minY)
+                    NSApp.activate(ignoringOtherApps: true)
+                    // popover.show(relativeTo: sbutton.bounds, of: sbutton, preferredEdge: NSRectEdge.minY)
                 }
             }
 
